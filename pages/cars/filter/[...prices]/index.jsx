ecfabf366cc652031db013909d04fd71ef7990e1
Alireza Fatemi <fa1381138100@gmail.com>
2023-11-04 22:29:10 +0330

    edited for deploy

--- pages/cars/filter/[...prices]/index.jsx
+++ pages/cars/filter/[...prices]/index.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 //DATA
-import carsData from '../../../data/carsData';
-import ShowFiltered from '../../../components/templates/ShowFiltered/ShowFiltered';
+import carsData from '../../../../data/carsData';
+import ShowFiltered from '../../../../components/templates/ShowFiltered/ShowFiltered';
 
 const FilterPrice = () => {
 
     const router = useRouter()
     const [ min ,max ] = router.query.prices;
     const filterCars = carsData.filter( car => car.price > min && car.price < max );
-    console.log(filterCars)
+    console.log(min,max)
 
-    if ( !filterCars.length ) return <h1>Notfound</h1>
+    if ( !filterCars.length ) return <h1 style={{ textAlign:"center" }} >Notfound</h1>
 
     return (
         <div>
